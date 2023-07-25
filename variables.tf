cf7286fdb32c495083487b9cc0070ac744940c9d
Christian Diaz <cdiaz@pxg.com>
2023-07-25 14:41:57 -0700

    Resolve fmt

--- variables.tf
+++ variables.tf
@@ -1,16 +1,16 @@
 variable "project_id" {
   description = "Google Cloud project ID"
-  default = "nextcloud-cdiaz-cloud"
+  default     = "nextcloud-cdiaz-cloud"
 }
 
 variable "region" {
   description = "Google Cloud region"
-  default = "us-west1"
+  default     = "us-west1"
 }
 
 variable "zone" {
   description = "Google Cloud zone"
-  default = "us-west1-b"
+  default     = "us-west1-b"
 }
 
 
