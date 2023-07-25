41f6dc888405e6e9b48ad4f35d794226bef95969
Christian Diaz <cdiaz@pxg.com>
2023-07-25 14:30:35 -0700

    Add providers to test GItlab CI

---
+++ main.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.75.0"
+    }
+    random = {
+      source = "hashicorp/random"
+      version = "3.5.1"
+    }
+  }
+}
