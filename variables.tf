e293db25f654149f037d86d12d9fb77133ee27db
Christian Diaz <cdiaz@pxg.com>
2023-07-25 17:23:48 -0700

    Define vars type

--- variables.tf
+++ variables.tf
@@ -1,16 +1,19 @@
 variable "project_id" {
   description = "Google Cloud project ID"
   default     = "nextcloud-cdiaz-cloud"
+  type        = string
 }
 
 variable "region" {
   description = "Google Cloud region"
   default     = "us-west1"
+  type        = string
 }
 
 variable "zone" {
   description = "Google Cloud zone"
   default     = "us-west1-b"
+  type        = string
 }
 
 
