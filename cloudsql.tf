2704176af1cf5fc53f95cc4ea77e0e4e0eef8ae2
Christian Diaz <cdiaz@pxg.com>
2023-07-27 10:56:47 -0700

    Use sql module from GCP

--- cloudsql.tf
+++ cloudsql.tf
@@ -1,49 +1,34 @@
-## Generate Random Passwords
-resource "random_password" "root_pwd" {
-  length  = 16
-  special = false
-}
-resource "random_password" "db_user_pw" {
-  length  = 16
-  special = false
-}
-
-## Database Instance
-resource "google_sql_database_instance" "nextcloud-db-instance" {
+module "sql-db" {
+  source              = "GoogleCloudPlatform/sql-db/google//modules/mysql"
   name                = "nextcloud-db-instance"
+  project_id          = var.project_id
   database_version    = "MYSQL_8_0"
-  project             = var.project_id
-  region              = var.region
   deletion_protection = true
-  root_password       = random_password.root_pwd.result
-  settings {
-    tier              = "db-f1-micro"
-    disk_type         = "PD_HDD"
-    disk_size         = 10
-    disk_autoresize   = false
-    availability_type = "ZONAL"
-    backup_configuration {
-      enabled                        = true
-      binary_log_enabled             = true
-      start_time                     = "01:00"
-      point_in_time_recovery_enabled = false
-      transaction_log_retention_days = 7
-      location                       = var.region
+
+  // Master Configuration
+  tier                            = "db-f1-micro"
+  zone                            = var.zone
+  region                          = var.region
+  availability_type               = "ZONAL"
+  maintenance_window_day          = 7
+  maintenance_window_hour         = 12
+  maintenance_window_update_track = "stable"
+  disk_autoresize                 = false
+  disk_type                       = "PD_HDD"
+  disk_size                       = 10
+
+  // Nextcloud Database
+  db_name      = "nextcloud-db"
+  db_charset   = "utf8mb4"
+  db_collation = "utf8mb4_general_ci"
+
+  additional_users = [
+    {
+      name            = "nextcloud"
+      host            = "localhost"
+      password        = ""
+      type            = "BUILT_IN"
+      random_password = true
     }
-  }
-}
-
-## Database
-resource "google_sql_database" "nextcloud-db" {
-  name      = "nextcloud-db"
-  instance  = google_sql_database_instance.nextcloud-db-instance.name
-  charset   = "utf8mb4"
-  collation = "utf8mb4_general_ci"
-}
-
-## Database User
-resource "google_sql_user" "nextcloud-db-user" {
-  name     = "nextcloud"
-  instance = google_sql_database_instance.nextcloud-db-instance.name
-  password = random_password.db_user_pw.result
+  ]
 }
